deab77ae2a8751e5e3552dede14b2315b1d1e355
V-gy <vgy@hotmail.fr>
2019-12-02 13:41:03 +0100

    Updates

---
+++ precache-manifest.19ba8267c22a6214581c870f09a2ff2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9334931fb09ae75458fb5c579af9b45e",
+    "url": "/ReactHooks/index.html"
+  },
+  {
+    "revision": "6177a8cc2ee67a31f0a7",
+    "url": "/ReactHooks/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "47b18aaf067b9755f57c",
+    "url": "/ReactHooks/static/js/2.ee7983f7.chunk.js"
+  },
+  {
+    "revision": "6177a8cc2ee67a31f0a7",
+    "url": "/ReactHooks/static/js/main.f6526acf.chunk.js"
+  },
+  {
+    "revision": "ce40fe3177cdecf3a596",
+    "url": "/ReactHooks/static/js/runtime-main.e8f38551.js"
+  }
+]);
